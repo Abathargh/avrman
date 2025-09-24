bcc5138256bcb0739653113e54bd0b2b840a4957
mar <gim.marcello@gmail.com>
2025-09-24 21:35:58 +0200

    added: first full `device` implementation.
    This is a major rework of the experimental device command, with added
    information on known programmers and boards. More can be added by
    modifying the boards.json and programmers.json files.

--- device/device.nim
+++ device/device.nim
@@ -4,7 +4,7 @@
 ## everything regarding the loading and discovery process, so that's what this
 ## is about, a generic mechanism for device and pid/vid retrieval.
 
-import std/[dirs, os, strutils, sets, sugar, tables]
+import std/[dirs, json, os, sequtils, strformat, strutils, sets, sugar, tables]
 
 ## a target must define ‘enumerate_serial_devices‘ and ‘get_vid_pid‘
 
@@ -21,69 +21,62 @@ else:
     raise new_exception(UnsupportedException, "unsupported platform")
 
 
-type Device = object
-  vid:  uint16
-  pid:  uint16
-  mcu:  string
-  opts: string
+type Device* = object
+  name:     string
+  protocol: string
+  mcu:      string
+  ids:      Table[uint16, OrderedSet[uint16]]
+  speed:    int
+  flush:    bool
 
 
 # this is a table with every interesting device that may be used to flash an
 # avr mcu
 
-const interesting_vids = {
-  0x3EB'u16: @[ # atmel
-    Device(name: "JTAG-ICE-mkII", pid: 0x2103),
-    Device(name: "AVR-ISP-mkII", pid: 0x2104),
-    Device(name: "AVRONE!", pid: 0x2105),
-    Device(name: "STK600", pid: 0x2106),
-    Device(name: "AVR Dragon", pid: 0x2107),
-    Device(name: "AVR JTAGICE3", pid: 0x2110),
-    Device(name: "AVR JTAGICE3-v3.x", pid: 0x2140),
-    Device(name: "ICE debugger", pid: 0x2141),
-    Device(name: "ATMEGA328P-XMINI", pid: 0x2145),
-  ],
-  0x2341: @[ # arduino
-    Device(name: "Uno", pid: 0x0001, mcu: "m328p", opts: "-b 11520"),
-    Device(name: "Mega2560", pid: 0x0010, mcu: "m2560", opts: "-D -b 11520"),
-    Device(name: "LeonardoBootloader", pid: 0x0036),
-    Device(name: "SerialAdapter", pid: 0x003b),
-    Device(name: "DueProgrammingPort", pid: 0x003d),
-    Device(name: "Due", pid: 0x003e),
-    Device(name: "MegaADK", pid: 0x003f),
-    Device(name: "Mega2560R3", pid: 0x0042, mcu: "m2560", opts: "-D -b 11520"),
-    Device(name: "UnoR3", pid: 0x0043, mcu: "m328p", opts: "-b 11520"),
-    Device(name: "MegaADKR3", pid: 0x0044),
-    Device(name: "SerialR3", pid: 0x0045),
-    Device(name: "ISP", pid: 0x0049),
-    Device(name: "Leonardo ", pid: 0x8036, mcu: "m32u4", opts: "-b 57600"),
-    Device(name: "RobotControl ", pid: 0x8038, mcu: "m32u4", opts: "-b 57600"),
-    Device(name: "RobotMotor ", pid: 0x8039, mcu: "m32u4", opts: "-b 57600"),
-  ],
-  0x1366: @[ # segger
-    Device(name: "J-Link PLUS", pid: 0x0101),
-    Device(name: "J-Link", pid: 0x1015),
-  ],
-}.toTable
+proc parse_ids(node: JsonNode): Table[uint16, OrderedSet[uint16]] =
+  for key, val in node.pairs:
+    var vid = key.parse_hex_int.uint16
+    for pid in val:
+      if vid notin result: result[vid] = initOrderedSet[uint16]()
+      result[vid].incl pid.get_str.parse_hex_int.uint16
+
+
+proc parse_dev(node: JsonNode): Device =
+  # mandatory fields
+  result.name     = get_str   node["name"]
+  result.protocol = get_str   node["protocol"]
+  result.ids      = parse_ids node["id_map"]
+
+  # non-mandatory fields
+  result.mcu      =     get_str(node.get_or_default("mcu"), "")
+  result.speed    =     get_int( node.get_or_default("speed"), -1)
+  result.flush    = not get_bool(node.get_or_default("dis_flush"), false)
+
+
+const devices* = (proc(): Table[string, Device] =
+  let boards = "device/boards.json".read_file.parse_json
+  let progs  = "device/programmers.json".read_file.parse_json
+  for board in boards:
+    let jboard = board.parse_dev
+    let tname  = jboard.name.to_lower_ascii.replace("-", "")
+    result[tname] = jboard
+
+  for prog in progs:
+    let jprog = prog.parse_dev
+    let tname = jprog.name.to_lower_ascii.replace("-", "")
+    result[tname]  = jprog
+)()
 
 # The following two here are just a const sequence of the names of the device
 # above, and a const concatenated version of the same, to print with
 # avrman device -l, for convenience
 
-const supported_devices* = collect(initHashSet()):
-  for device_list in interesting_vids.values:
-    for device in device_list:
-      {device.name}
+const supported_names* = collect(initHashSet()):
+  for name in devices.keys:
+    {name}
 
 
-const supported_devices_str* = (
-  block:
-    var dev_str: seq[string] = @[]
-    for device_list in interesting_vids.values:
-      for device in device_list:
-        dev_str.add(device.name)
-    dev_str
-).join("\n")
+const supported_names_str* = supported_names.to_seq.join("\n")
 
 
 proc levenshtein(s1, s2: string): int =
@@ -107,24 +100,38 @@ proc levenshtein(s1, s2: string): int =
 
   supp[s1.len][s2.len]
 
+
 proc closest_guess*(name: string): string =
   ## levenshtein distance for the supported devices
   var min_name = ""
   var min_len  = int.high
 
-  for device_name in supported_devices:
+  for device_name in supported_names:
     let distance = levenshtein(name, device_name)
     if distance < min_len:
       min_name = device_name
       min_len = distance
-
   min_name
+
+
+proc same_name(s1, s2: string): bool =
+  let trim1 = s1.to_lower_ascii.replace("_", "")
+  trim1 == s2
+
 
 proc find_device_port*(name: string): string =
   for port in enumerate_serial_devices():
     let (vid, pid) = get_vid_pid(port)
-    if vid in interesting_vids:
-      for device in interesting_vids[vid]:
-        if pid == device.pid and name == device.name:
-          return port
+    for dev_name, dev in devices.pairs:
+      if vid in dev.ids and pid in dev.ids[vid] and same_name(name, dev_name):
+        return port
   ""
+
+
+proc get_device_config*(name: string): string =
+  let dev = devices[name]
+  var conf_str = fmt"-c {dev.protocol}"
+  if dev.mcu   != "": conf_str.add fmt" -p {dev.mcu}"
+  if dev.speed != -1: conf_str.add fmt" -b {dev.speed}"
+  if dev.flush:       conf_str.add fmt" -D"
+  conf_str
